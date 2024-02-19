9b62b90f42d097bc37b44eeb9a982b5cc59a7bb7
Martin Partida <mrtnprtd@gmail.com>
2024-02-18 23:07:27 -0800

    Added website/menu resizing for mobile view

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello, World!');
+console.log("Hello, World!");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
